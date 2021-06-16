8ff6b8dab5a1d1f29ad7e9b7deae4174c3c3339d
Damian Demasi <damian.demasi.1@gmail.com>
2021-06-17 09:26:49 +0930

    Fundamentals - assignment

--- 01-fundamentals-1-script.js
+++ 01-fundamentals-1-script.js
@@ -3,3 +3,19 @@ console.log('This is coming from an external script!');
 
 let myName = 'Damian';
 console.log(myName);
+
+// Testing var and let
+let thisIsLet;
+var thisIsVar;
+const THISISCONST = 0; // Must be initialised when defined.
+thisIsGlobal = ''; // Must be initialised when defined.
+console.log(thisIsLet);
+console.log(typeof thisIsLet);
+console.log(thisIsVar);
+console.log(typeof thisIsVar);
+console.log(THISISCONST);
+console.log(typeof THISISCONST);
+console.log(thisIsGlobal);
+console.log(typeof thisIsGlobal);
+
+console.log(typeof null);
