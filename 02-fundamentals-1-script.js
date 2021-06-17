fff96a9432b06953572d3b0b9a95ba6d38732865
Damian Demasi <damian.demasi.1@gmail.com>
2021-06-17 16:22:55 +0930

    Testing 'use strict' mode

---
+++ 02-fundamentals-1-script.js
@@ -0,0 +1,7 @@
+'use strict'; // Needs to be before any code in the script
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicens = true; // Error introduced to check the 'use strict' functionality (comment out 'use strict' and compare errors)
+if (hasDriversLicense) console.log('I can drive! 🚗');
