6f8cfd2623233a61e8bc5fcda1d34cd86b9d6066
Damian Demasi <damian.demasi.1@gmail.com>
2021-06-18 11:06:24 +0930

    Objects pracitce

--- 02-fundamentals-1-script.js
+++ 02-fundamentals-1-script.js
@@ -3,5 +3,20 @@
 let hasDriversLicense = false;
 const passTest = true;
 
-if (passTest) hasDriversLicens = true; // Error introduced to check the 'use strict' functionality (comment out 'use strict' and compare errors)
+// if (passTest) hasDriversLicens = true; // Error introduced to check the 'use strict' functionality (comment out 'use strict' and compare errors)
+if (passTest) hasDriversLicense = true; // Error introduced to check the 'use strict' functionality (comment out 'use strict' and compare errors)
 if (hasDriversLicense) console.log('I can drive! 🚗');
+
+// Objects
+const damianObject = {
+    // Properties
+    firstName: 'Damian',
+    lastName: 'Demasi',
+    birthYear: 1981, // 👴
+
+    // Methods
+    age: function (currentYear) { return currentYear - damianObject.birthYear; },
+    fullName: (firstName, lastName) => `${firstName} ${lastName}`
+}
+
+console.log(damianObject.age(2021), '👴');
