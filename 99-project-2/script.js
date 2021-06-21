65bf7b96c313996cba47035dbacd4d56cffcc572
Damian Demasi <damian.demasi.1@gmail.com>
2021-06-22 08:05:52 +0930

    Add and remove classes on event listeners

--- 99-project-2/script.js
+++ 99-project-2/script.js
@@ -7,5 +7,19 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 console.log(btnsOpenModal);
 
+const closeModal = () => {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+const openModal = () => {
+    modal.classList.remove('hidden'); // Do not use the dot here
+    overlay.classList.remove('hidden'); // Do not use the dot here
+}
+
 for (let i = 0; i < btnsOpenModal.length; i++)
-    console.log(btnsOpenModal[i].textContent);
+    btnsOpenModal[i].addEventListener('click', openModal); // Do not use parenthesis (call the function) because we need the actual function, not its value
+
+btnCloseModal.addEventListener('click', closeModal); // Do not use parenthesis (call the function) because we need the actual function, not its value
+
+overlay.addEventListener('click', closeModal); // Do not use parenthesis (call the function) because we need the actual function, not its value
