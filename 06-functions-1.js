f6653da766e51d7ad1a3d024ee1100cd25a128ec
Damian Demasi <damian.demasi.1@gmail.com>
2021-06-29 09:49:05 +0930

    Passing arguments: value vs reference

--- 06-functions-1.js
+++ 06-functions-1.js
@@ -24,3 +24,34 @@ createBooking('LH123', 2, 800);
 createBooking('LH123', 2);
 createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000); // Skipping an argument (ES6)
+
+// SECTION *** Passing arguments: value vs reference ***
+
+const flight = 'LH456';
+const damian = {
+    fullName: 'Damian Demasi',
+    passport: 123456789
+};
+
+const checkIn = function (flightNum, passenger) {
+    flightNum = 'LH999'; // Flight was reassigned by external source
+    passenger.name = 'Mr. ' + damian.fullName; // Mutable: a modification in the copy of an object will have impact in the original object
+
+    if (passenger.passport === 123456789) { // Passport number from external source
+        alert('Checked in!');
+    } else {
+        alert('Wrong passport!');
+    }
+};
+
+checkIn(flight, damian);
+console.log(flight, damian); // flight stays the same, whilst damian gets updated
+
+const newPassport = function (person) {
+    person.passport = 100000000 + Math.trunc(Math.random() * 100000000);
+}
+
+newPassport(damian); // WARNING: in this case, the same object is being modified by two functions, something that should be avoided
+// When we pass an object to a function as an argument, it looks like we are passing a reference, but actually is a value (is the memory address of the object in the Heap, which looks like a reference).
+checkIn(flight, damian);
+console.log(flight, damian);
