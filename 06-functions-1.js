9611107ac4a71b2e75e986180cd9be51a2cec950
Damian Demasi <damian.demasi.1@gmail.com>
2021-06-29 10:35:50 +0930

    First-Class and Higher-Order Functions - abstraction

--- 06-functions-1.js
+++ 06-functions-1.js
@@ -55,3 +55,42 @@ newPassport(damian); // WARNING: in this case, the same object is being modified
 // When we pass an object to a function as an argument, it looks like we are passing a reference, but actually is a value (is the memory address of the object in the Heap, which looks like a reference).
 checkIn(flight, damian);
 console.log(flight, damian);
+
+// SECTION *** First-Class and Higher-Order Functions ***
+
+// First-class functions
+const oneWord = function (str) {
+    return str.replace(/ /g, '').toLowerCase();
+}
+
+const upperFirstWord = function (str) {
+    const [first, ...others] = str.split(' ');
+    return [first.toUpperCase(), ...others].join(' ');
+}
+
+// Higher-order function
+const transformer = function (str, fn) {
+    console.log(`Original string:    ${str}`);
+    console.log(`Transformed string: ${fn(str)}`);
+    console.log(`Transformed by: ${fn.name}`); // Function property (because they are objects)
+}
+
+transformer('JavaScript is the best!', upperFirstWord);
+/* ->
+Original string:    JavaScript is the best!
+Transformed string: JAVASCRIPT is the best!
+Transformed by: upperFirstWord
+*/
+
+transformer('JavaScript is the best!', oneWord);
+/* ->
+Original string:    JavaScript is the best!
+Transformed string: javascriptisthebest!
+Transformed by: oneWord
+*/
+
+const high5 = function () {
+    alert('🖐');
+}
+
+document.body.addEventListener('click', high5); // The higher-order function (addEventListener) calls the event handler callback function (high5)
