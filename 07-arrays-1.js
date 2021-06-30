71e821ef3de97cdb126403a5e6dc52af8b218128
Damian Demasi <damian.demasi.1@gmail.com>
2021-06-30 12:01:00 +0930

    forEach on Maps and Sets

--- 07-arrays-1.js
+++ 07-arrays-1.js
@@ -65,11 +65,11 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
@@ -197,4 +197,33 @@ Movement 5: You withdrew 650
 Movement 6: You withdrew 130
 Movement 7: You deposited 70
 Movement 8: You deposited 1300
+*/
+
+// SECTION *** forEach on Maps and Sets ***
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach((value, key, map) => {
+  console.log(`${key}: ${value}`);
+});
+/* ->
+USD: United States dollar
+EUR: Euro
+GBP: Pound sterling
+*/
+
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+
+currenciesUnique.forEach(function (value, key, set) { // The value and the key are the same because sets don't have keys or indexes
+  console.log(`${key}: ${value}`);
+});
+/* ->
+USD: USD
+GBP: GBP
+EUR: EUR
 */
